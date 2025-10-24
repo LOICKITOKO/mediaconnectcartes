cf82da11992ce7bbb4e121d2c9f1c2d9667a4cbd
LOICKITOKO <loicassaelkm@gmail.com>
2025-10-24 21:34:58 +0200

    all task

--- cartes/models.py
+++ cartes/models.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from django.core.files import File
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 class Carte(models.Model):
     card_id = models.CharField(max_length=50, unique=True)
@@ -16,15 +17,25 @@ class Carte(models.Model):
     qr_code = models.ImageField(upload_to='qr_codes/', blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        # Expiration automatique à +1 an si vide
+        # Définir une expiration à +1 an si vide
         if not self.expiration_date:
             self.expiration_date = self.issued_at.replace(year=self.issued_at.year + 1)
 
         # Générer QR code
-        qr_url = f"https://TONSERVICE.onrender.com/verify/?card={self.card_id}"
+        # Détecte si DEBUG pour choisir l'URL
+        if settings.DEBUG:
+            base_url = 'http://127.0.0.1:8000'
+        else:
+            base_url = 'https://mediaconnectcartes.onrender.com'
+
+        qr_url = f"{base_url}/verify/?card={self.card_id}"
+
+        # Génération de l'image QR
         qr_img = qrcode.make(qr_url)
         buffer = BytesIO()
         qr_img.save(buffer, format='PNG')
+
+        # Sauvegarde dans le champ qr_code
         self.qr_code.save(f"{self.card_id}.png", File(buffer), save=False)
 
         super().save(*args, **kwargs)
